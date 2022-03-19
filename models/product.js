557f89b821771ff2e76c7aa4b0881a74b1f01ee3
mrdeangray <mrdeangray@gmail.com>
2022-03-19 07:43:36 -0400

    add product schema

---
+++ models/product.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+const productSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        require: true
+
+    },
+    price:{
+        type: Number,
+        required: true,
+        min: 0
+    },
+    category:{
+        type: String,
+        enum:['fruit', 'vegetable', 'dairy']
+    }
+})
